f629710517f3021cc52a32e5c96e6d98ab80db00
AidanLGregory <40250566+AidanLGregory@users.noreply.github.com>
2022-10-30 15:38:44 +0700

    Basic setup
    Installed tailwind + configured - Made first react component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <section id="intro" className="flex items-center justify-center flex-col pt-20 pb-6">
+            <div>
+                <h1>Aidan Gregory</h1>
+                <p>Freelance Developer &#38; Networking Engineer</p>
+            </div>
+        </section>
+    );
+}
+
+export default Intro;
